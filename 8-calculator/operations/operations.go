187e9c250dc1f6829d0464c3c3e6a39c33dc78a4
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-12-12 16:01:39 -0300

    adding some comments

--- 8-calculator/operations/operations.go
+++ 8-calculator/operations/operations.go
@@ -1,8 +1,8 @@
 package operations
 
-/* import (
-
-) */
+// this file belongs to the operations package I created with the oly purpose of providing the
+// following functions as the packages utility, which I import from another file and make use of them
+// as you can see there is no main function and these functions serve individual help only
 
 func Add(a int, b int) int {
 	return a + b
