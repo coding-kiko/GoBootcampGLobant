9f4c93d55b6fd9f9e636c7ac7df11b5679160333
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-12-13 18:42:55 -0300

    testing my error library

--- 12-errorPackage/MyErrors/myErrors.go
+++ 12-errorPackage/MyErrors/myErrors.go
@@ -6,8 +6,38 @@ import (
 	"os"
 )
 
-type Internal struct{}
+type Internal struct {
+	description string
+}
 
-type ThirdParty struct{}
+func (i *Internal) Error() string {
+    return fmt.Println("Error: ThirdParty:", i.description)
+}
 
-type Other struct{}
+type ThirdParty struct {
+	description string
+}
+
+func (t *ThirdParty) Error() string {
+    return fmt.Println("Error: ThirdParty:", t.description)
+}
+
+type Other struct {
+	description string
+}
+
+func (o *Other) Error() string {
+    return fmt.Println("Error: ThirdParty:", o.description)
+}
+
+// function that accepts any type of error and prints its type
+func CheckErrType(e error) {
+	switch tp := e.(type) {
+	case *ThirdParty:
+		fmt.Println("This error is a ThirdParty error")
+	case *Internal:
+		fmt.Println("This error is an Internal error")
+	default:
+		fmt.Println("This error is a different type of error (Other)")
+	}
+}
