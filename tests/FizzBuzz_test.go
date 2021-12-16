00875fa5b8b1f6a1f0aacef4e853e1dca6ee5125
coding-kiko <2789@holbertonschool.com>
2021-12-16 15:06:03 -0300

    0 case test, negative number case

--- tests/FizzBuzz_test.go
+++ tests/FizzBuzz_test.go
@@ -8,6 +8,9 @@ import (
 )
 
 func FizzBuzz(n int) string {
+	if n < 0 {
+		return "Invalid number"
+	}
 	switch {
 	case n%5 == 0 && n%3 == 0:
 		return "FizzBuzz"
@@ -20,6 +23,7 @@ func FizzBuzz(n int) string {
 	}
 }
 
+// I will implement test tables as my choice for this exercise
 func TestFizzBuzz(t *testing.T) {
 	testcases := []struct { // create test cases struct
 		name     string // string description
@@ -34,8 +38,10 @@ func TestFizzBuzz(t *testing.T) {
 		{name: "validate multiple of 3 and 5", input: 60, expected: "FizzBuzz"},
 		{name: "validate multiple of none", input: 8, expected: "8"},
 		{name: "validate multiple of none", input: 88, expected: "88"},
+		{name: "0 case", input: 0, expected: "FizzBuzz"},
+		{name: "negative number", input: -2, expected: "Invalid number"},
 	}
-	for _, tCase := range testcases { // iterate over every wantes test case
+	for _, tCase := range testcases { // iterate over every wanted test case
 		t.Run(tCase.name, func(t *testing.T) {
 			result := FizzBuzz(tCase.input)
 			assert.Equal(t, tCase.expected, result) // check if result is the same as expected
